092a518f0a6af567418eb95beb3ab01be9d5bd04
tepidjuice <tamemeese@gmail.com>
2017-10-13 12:02:01 +1100

    Update start-temp-target.sh

--- start-temp-target.sh
+++ start-temp-target.sh
@@ -3,9 +3,10 @@
 OPENAPSROOT="/root/myopenaps"
 REASON=$1
 TARGET=$2
+DURATION=$3
 
 echo "\
-[{\"_id\":\"\",\"enteredBy\":\"\",\"eventType\":\"Temporary Target\",\"reason\":\"$REASON\",\"targetTop\":$TARGET,\"targetBottom\":$TARGET,\"duration\":60,\"created_at\":\"$(date --utc +'%Y-%m-%dT%H:%M:%S.000Z')\",\"carbs\":null,\"insulin\":null}] \
+[{\"_id\":\"\",\"enteredBy\":\"\",\"eventType\":\"Temporary Target\",\"reason\":\"$REASON\",\"targetTop\":$TARGET,\"targetBottom\":$TARGET,\"duration\":DURATION,\"created_at\":\"$(date --utc +'%Y-%m-%dT%H:%M:%S.000Z')\",\"carbs\":null,\"insulin\":null}] \
 " > $OPENAPSROOT/settings/temptargets.json
 cd $OPENAPSROOT
 openaps report invoke settings/profile.json
